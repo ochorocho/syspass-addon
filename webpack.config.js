6397d7f36f26d48e6f63d6e13518981d0a37bce3
Jochen <rothjochen@gmail.com>
2020-01-15 00:10:40 +0100

    Add yarn, webpack config and build scripts

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        syspass: "./syspass.js"
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "index.js"
+    },
+    watchOptions: {
+        ignored: ['dist', 'node_modules']
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            {from: 'addon', to: './'}
+        ]),
+    ]
+};
